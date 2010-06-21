f88651037d93498946feb2d37979f12c070897b1
dfilatov <alpha@zforms.ru>
2010-06-21 20:50:03 +0000

    inheritance plugin updated

--- js/jquery/jquery.inherit.js
+++ js/jquery/jquery.inherit.js
@@ -1,28 +1,76 @@
 /**
  * Inheritance plugin
  *
- * Copyright (c) 2009 Filatov Dmitry (alpha@zforms.ru)
+ * Copyright (c) 2010 Filatov Dmitry (alpha@zforms.ru)
  * Dual licensed under the MIT and GPL licenses:
  * http://www.opensource.org/licenses/mit-license.php
  * http://www.gnu.org/licenses/gpl.html
  *
- * @version 1.1.1
+ * @version 1.3.2
  */
 
 (function($) {
 
 var hasIntrospection = (function(){_}).toString().indexOf('_') > -1,
+	needCheckProps = $.browser.msie, // fucking ie hasn't toString, valueOf in for
+	specProps = needCheckProps? ['toString', 'valueOf'] : null,
 	emptyBase = function() {};
+
+function override(base, result, add) {
+
+	var hasSpecProps = false;
+	if(needCheckProps) {
+		var addList = [];
+		$.each(specProps, function() {
+			add.hasOwnProperty(this) && (hasSpecProps = true) && addList.push({
+				name : this,
+				val  : add[this]
+			});
+		});
+		if(hasSpecProps) {
+			$.each(add, function(name) {
+				addList.push({
+					name : name,
+					val  : this
+				});
+			});
+			add = addList;
+		}
+	}
+
+	$.each(add, function(name, prop) {
+		if(hasSpecProps) {
+			name = prop.name;
+			prop = prop.val;
+		}
+		if($.isFunction(base[name]) && $.isFunction(prop) &&
+		   (!hasIntrospection || prop.toString().indexOf('.__base') > -1)) {
+
+			var baseMethod = base[name];
+			result[name] = function() {
+				var baseSaved = this.__base;
+				this.__base = baseMethod;
+				var result = prop.apply(this, arguments);
+				this.__base = baseSaved;
+				return result;
+			};
+
+		}
+		else {
+			result[name] = prop;
+		}
+
+	});
+
+}
 
 $.inherit = function() {
 
-	var withMixins = $.isArray(arguments[0]),
-		hasBase = $.isFunction(arguments[0]) || withMixins,
-		base = hasBase? withMixins? arguments[0][0] : arguments[0] : emptyBase,
-		basePtp = base.prototype,
+	var hasBase = $.isFunction(arguments[0]),
+		base = hasBase? arguments[0] : emptyBase,
 		props = arguments[hasBase? 1 : 0] || {},
 		staticProps = arguments[hasBase? 2 : 1],
-		result = props.__constructor || basePtp.__constructor?
+		result = props.__constructor || (hasBase && base.prototype.__constructor)?
 			function() {
 				this.__constructor.apply(this, arguments);
 			} : function() {};
@@ -33,70 +81,17 @@ $.inherit = function() {
 		return $.extend(result, staticProps);
 	}
 
-	var inheritance = function() {};
+	$.extend(result, base);
 
-	$.extend(result, base, staticProps);
-
-	inheritance.prototype = basePtp;
+	var inheritance = function() {},
+		basePtp = base.prototype;
+	inheritance.prototype = base.prototype;
 	result.prototype = new inheritance();
-    var resultPtp = result.prototype;
+	var resultPtp = result.prototype;
 	resultPtp.__self = resultPtp.constructor = result;
 
-	var propList = [];
-	$.each(props, function(i) {
-		props.hasOwnProperty(i) && propList.push(i);
-	});
-	// fucking ie hasn't toString, valueOf in for
-	$.each(['toString', 'valueOf'], function() {
-		props.hasOwnProperty(this) && $.inArray(this, propList) == -1 && propList.push(this);
-	});
-
-	var propName, i = 0;
-	while(propName = propList[i++]) {
-		if(hasBase
-			&& $.isFunction(basePtp[propName]) && $.isFunction(props[propName])
-			&& (!hasIntrospection || props[propName].toString().indexOf('.__base') > -1)) {
-
-			(function(methodName) {
-				var baseMethod = basePtp[methodName],
-					overrideMethod = props[methodName];
-				resultPtp[methodName] = function() {
-					var baseSaved = this.__base;
-					this.__base = baseMethod;
-					var result = overrideMethod.apply(this, arguments);
-					this.__base = baseSaved;
-					return result;
-				};
-			})(propName);
-
-		}
-		else {
-			resultPtp[propName] = props[propName];
-		}
-	};
-
-	if(withMixins) {
-		var i = 1, mixins = arguments[0], mixin, __constructors = [];
-		while(mixin = mixins[i++]) {
-			$.each(mixin.prototype, function(propName) {
-				if(propName == '__constructor') {
-					__constructors.push(this);
-				}
-				else if(propName != '__self') {
-					resultPtp[propName] = this;
-				}
-			});
-		}
-		if(__constructors.length > 0) {
-			resultPtp.__constructor && __constructors.push(resultPtp.__constructor);
-			resultPtp.__constructor = function() {
-				var i = 0, __constructor;
-				while(__constructor = __constructors[i++]) {
-					__constructor.apply(this, arguments);
-				}
-			};
-		}
-	}
+	override(basePtp, resultPtp, props);
+	staticProps && override(base, result, staticProps);
 
 	return result;
 
